ace1717924216dd45b9efbd1e5dc541b138175f9
raven2r <ymassakovskyi@gmail.com>
2024-02-06 13:34:51 +0100

    Added 2 forgotten files

---
+++ src/main/java/me/raven2r/grevoc/core/translator/Deepl.java
@@ -0,0 +1,37 @@
+package me.raven2r.grevoc.core.translator;
+
+import com.deepl.api.Translator;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class Deepl implements Translates {
+    // make in external file
+    private String authKey = null;
+    private Translator translator = null;
+    private String translatedText = "";
+
+    Deepl() {
+        Properties properties = new Properties();
+
+        try {
+            properties.load(getClass().getClassLoader().getResourceAsStream("auth.properties"));
+            translator = new Translator(properties.getProperty("deepl.api.key"));
+        }
+        catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+    @Override
+    public String translate(String source, String target, String text) {
+        try {
+            translatedText = translator.translateText(text, source, target).getText();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return translatedText;
+    }
+}
---
+++ src/main/java/me/raven2r/grevoc/core/translator/Translates.java
@@ -0,0 +1,5 @@
+package me.raven2r.grevoc.core.translator;
+
+public interface Translates {
+    public String translate(String source, String target, String text);
+}
